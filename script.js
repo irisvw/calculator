48f7da4714c4f51117f958061afaa7c8ebb91ebc
Iris van Willigen <iris.van.willigen@gmail.com>
2024-01-11 12:09:05 +0100

    add basic functions

---
+++ script.js
@@ -0,0 +1,29 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "+";
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+': return add(a, b);
+        case '-': return subtract(a, b);
+        case '*': return multiply(a, b);
+        case '/': return divide(a, b);
+        default: return "error";
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
