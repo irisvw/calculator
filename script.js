38d82ade72365e3a2410f0f95c634478bcb8a008
Iris van Willigen <iris.van.willigen@gmail.com>
2024-01-12 11:34:54 +0100

    add operating ability

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let firstNumber = 0;
-let secondNumber = 0;
+let firstNumber;
+let secondNumber;
 let operator = "+";
+let result = "";
 
 function operate(operator, a, b) {
+    a = parseInt(a);
+    b = parseInt(b);
     switch (operator) {
         case '+': return add(a, b);
         case '-': return subtract(a, b);
@@ -30,4 +33,103 @@ function divide(a, b) {
 
 // display
 
-let display = ""
+function pressNumberButton(target) {
+    switch (target.id) {
+        case 'button0':
+            return 0;
+        case 'button1':
+            return 1;
+        case 'button2':
+            return 2;
+        case 'button3':
+            return 3;
+        case 'button4':
+            return 4;
+        case 'button5':
+            return 5;
+        case 'button6':
+            return 6;
+        case 'button7':
+            return 7;
+        case 'button8':
+            return 8;
+        case 'button9':
+            return 9;
+        default: return "";
+    }
+}
+
+function pressOperatorButton(target) {
+    switch (target.id) {
+        case 'button+':
+            return "+";
+        case 'button-':
+            return "-";
+        case 'button*':
+            return "*";
+        case 'button/':
+            return "/";
+    }
+}
+
+let display = "";
+let calculatorField = document.querySelector(".calculator");
+let calculatorDisplay = document.querySelector("#display");
+
+calculatorField.addEventListener('click', (event) => {
+    let target = event.target;
+    switch (target.className) {
+        case "numberButton": {
+            if (result) {
+                display = "";
+                calculatorDisplay.innerHTML = display;
+                result = "";
+            }
+            buttonPressed = pressNumberButton(target);
+            display += buttonPressed;
+            calculatorDisplay.innerHTML = display;
+            break;
+        }
+        case "operateButton": {
+            if (!firstNumber) {
+                firstNumber = display;
+                display = "";
+                calculatorDisplay.innerHTML = display;
+                operator = pressOperatorButton(target);
+            } else {
+                secondNumber = display;
+                display = operate(operator, firstNumber, secondNumber);
+                firstNumber = "";
+                secondNumber = "";
+                calculatorDisplay.innerHTML = display;
+            }
+            break;
+        }
+        case "evaluateButton": {
+            if (firstNumber) {
+                secondNumber = display;
+                result = operate(operator, firstNumber, secondNumber);
+                calculatorDisplay.innerHTML = result;
+                firstNumber = "";
+                secondNumber = "";
+            }
+            break;
+        }
+        case "clearButton": {
+            firstNumber = "";
+            secondNumber = "";
+            operator = "";
+            display = "";
+            calculatorDisplay.innerHTML = display;
+            break;
+        }
+    }
+})
+
+// if operateButton is pressed, and firstNumber and secondNumber have already been given, operate
+// firstNumber and secondNumber first, and display the answer.
+
+// if numberButton is pressed, after a calculation, clear display and start new calculation.
+// if operateButton is pressed after a calculation, take result as firstNumber, take the operator, and prompt for secondNumber.
+
+// display += buttonPressed works correctly when display is a string, but not when display is an int.
